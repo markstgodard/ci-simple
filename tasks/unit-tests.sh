19427de31c284d980cd6f97de3e2f2a0096063c1
Mark St.Godard <markst.godard@boldcommerce.com>
2017-05-21 18:45:42 -0500

    use composer

--- tasks/unit-tests.sh
+++ tasks/unit-tests.sh
@@ -4,9 +4,6 @@ set -e
 echo "Running unit tests"
 php -v
 
-wget https://getcomposer.org/installer
-mv composer.phar /usr/local/bin/composer
-
 echo "Install dependencies..."
 composer install --dev --no-progress --no-interaction
 
