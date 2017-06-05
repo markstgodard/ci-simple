5b9c9ce020f656124fac6e452b97a43410be3982
Mark St.Godard <markstgodard@gmail.com>
2017-06-05 15:27:42 -0500

    rolling update

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -20,4 +20,17 @@ gcloud config set project $GCP_PROJECT
 gcloud container clusters get-credentials $GCP_CLUSTER_NAME --zone=$GCP_AZ
 gcloud config set container/cluster $GCP_CLUSTER_NAME
 
-kubectl run phpapp --image=markstgodard/phpapp --replicas=1 --port=80
+
+echo "Check if deployment exists"
+set +e
+kubectl get deployment phpapp
+rc=$?
+set -e
+
+if [ "$rc" = "0" ]; then
+  echo "Deployment already exists, update image"
+  kubectl rolling-update phpapp --image=$IMAGE_NAME
+else
+  echo "Create new deployment"
+  kubectl run phpapp --image=markstgodard/phpapp --replicas=1 --port=80
+fi
