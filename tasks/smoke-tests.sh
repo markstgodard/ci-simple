c2ea3da050d8fe24ca22543c0b7cdd21b91a0a12
Mark St.Godard <markstgodard@gmail.com>
2017-06-05 16:05:59 -0500

    smoke tests

--- tasks/smoke-tests.sh
+++ tasks/smoke-tests.sh
@@ -3,6 +3,28 @@ set -e
 
 echo "Smoke tests.."
 
-sleep $[ ( $RANDOM % 5 )  + 1 ]s
+tmp_dir=$(mktemp -d nevermind.XXXXXXX)
+echo $JSON_KEY >> $tmp_dir/key.json
+gcloud auth activate-service-account --key-file $tmp_dir/key.json
+
+gcloud config set project $GCP_PROJECT
+gcloud container clusters get-credentials $GCP_CLUSTER_NAME --zone=$GCP_AZ
+gcloud config set container/cluster $GCP_CLUSTER_NAME
+
+podname=`kubectl get pods --no-headers | cut -d' ' -f1`
+
+kubectl port-forward phpapp 8080:80 > /dev/null &
+
+statusCode=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8080)
+
+echo "Status code: " $statusCode
+
+if [ "$statusCode" = "200" ]; then
+  echo "Smoke tests passed"
+else
+  echo "Smoke tests failed"
+  exit 1
+fi
+
 
 echo "done"
