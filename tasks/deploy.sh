ca917efff387742efb5c78621075c63af4bc222a
Mark St.Godard <markst.godard@boldcommerce.com>
2017-06-05 14:30:05 -0500

    k8s deploy

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -3,8 +3,22 @@ set -e
 
 echo "Deploying.."
 
+echo "GCP_PROJECT:" $GCP_PROJECT
+echo "GCP_CLUSTER_NAME:" $GCP_CLUSTER_NAME
+echo "GCP_AZ:" $GCP_AZ
+echo "K8S_NAMESPACE:" $K8S_NAMESPACE
 
-kubectl version
+echo "Authenticate with GCP.."
+
+tmp_dir=$(mktemp -d nevermind.XXXXXXX)
+echo $JSON_KEY >> $tmp_dir/key.json
+gcloud auth activate-service-account --key-file $tmp_dir/key.json
+
+gcloud config set project $GCP_PROJECT
+gcloud container clusters get-credentials $GCP_CLUSTER_NAME --zone=$GCP_AZ
+gcloud config set container/cluster $GCP_CLUSTER_NAME
+
+kubectl create -f tasks/pod.yml -n $K8S_NAMESPACE
 
 
 echo "done"
